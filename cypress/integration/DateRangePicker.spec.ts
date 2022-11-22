9c4de4b5dee392cc1094a9dfd8d81f23d030cf07
rafat <rafatk@xcroco.com>
2022-11-22 21:43:39 +0100

    first commit

--- cypress/integration/DateRangePicker.spec.ts
+++ cypress/integration/DateRangePicker.spec.ts
@@ -1,6 +1,39 @@
+import { MuiTextField, MonthLabel } from '../test data/TestData.js';
+
 describe('DateRangePicker', () => {
   it('should render', () => {
     cy.visit('/');
     cy.get('.DateRangeSelect').should('be.visible');
+  });
+  it('should dispaly the calendar', () => {
+    cy.get(MuiTextField).eq(0).click();
+    cy.get(MonthLabel)
+      .should('contain', 'November 2022')
+      .and('contain', 'December 2022');
+  });
+
+  it('should allow users to select an old date range', () => {
+    cy.get('[data-testid="ArrowLeftIcon"]').eq(0).click();
+    cy.get('[aria-label="Oct 2, 2022"]').eq(0).click();
+    cy.get('[aria-label="Oct 7, 2022"]').click();
+    cy.checkDatesFields('[value="10/02/2022"]', '[value="10/07/2022"]');
+  });
+  it('should allow users to navigate between the months', () => {
+    cy.get('[data-testid="ArrowRightIcon"]').eq(1).click().click();
+    cy.get(MonthLabel)
+      .should('contain', 'December 2022')
+      .and('contain', 'January 2023');
+  });
+
+  it('should allow users to select a date range', () => {
+    cy.get('[aria-label="Jan 7, 2023"]').eq(0).click();
+    cy.get('[aria-label="Jan 14, 2023"]').click();
+    cy.checkDatesFields('[value="01/07/2023"]', '[value="01/14/2023"]');
+  });
+
+  it('should allow users to extend a selected date', () => {
+    cy.get('body').click('bottomRight');
+    cy.get(MuiTextField).eq(1).click();
+    cy.get('[aria-label="Jan 28, 2023"]').click();
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('checkDatesFields', (BeginDate, EndDate) => {
+    cy.get(BeginDate);
+    cy.get(EndDate);
+  });
+  
